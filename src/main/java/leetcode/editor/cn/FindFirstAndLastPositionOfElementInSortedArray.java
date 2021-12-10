5eff1364f5623643ae3258bccbb5faa48d9b951d
pengjizhi <498036762@qq.com>
2021-12-11 00:53:10 +0800

    修改《在排序数组中寻找元素的第一个和最后一个位置》解法，采用两次二分查找分别寻找最左侧和最右侧。

--- src/main/java/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -44,6 +44,7 @@
 */
 package leetcode.editor.cn;
 
+import java.lang.annotation.Target;
 import java.util.Arrays;
 
 //Java：在排序数组中查找元素的第一个和最后一个位置
@@ -51,10 +52,17 @@ public class FindFirstAndLastPositionOfElementInSortedArray{
     public static void main(String[] args) {
         Solution solution = new FindFirstAndLastPositionOfElementInSortedArray().new Solution();
         // TO TEST
-        int[] nums = {3,3};
+        int[] nums = {3,3,3,4,5,6,6,6};
         int target = 3;
         int[] result = solution.searchRange(nums, target);
         System.out.println(Arrays.toString(result));
+
+        int testLeft = solution.binaryLeft(nums, target);
+        System.out.println("testLeft:" + testLeft);
+        System.out.println("------");
+        int testRight = solution.binaryRight(nums, target);
+        System.out.println("testRight:" + testRight);
+
     }
     
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -70,31 +78,95 @@ class Solution {
                 return new int[] {-1, -1};
             }
         }
-        int left = 0, right = nums.length;
-        int resStart = -1, resEnd = -1;
+
+        int leftBorder = binaryLeft(nums, target);
+        int rightBorder = binaryRight(nums, target);
+        return new int[] {leftBorder, rightBorder};
+
+    }
+
+
+    public int binaryLeft(int[] nums, int target){
+        int left = 0;
+        int right = nums.length;
         while (left < right){
             int mid = left + (right - left >> 1);
-            if (target < nums[mid]){
+            if (target <= nums[mid]){
                 right = mid;
-            } else if (target > nums[mid]){
-                left = mid + 1;
             } else {
-                resStart = mid;
-                resEnd = mid;
-                while (resEnd + 1 < nums.length && nums[resEnd] == nums[resEnd + 1]){
-                    resEnd += 1;
-                }
-                while (resStart - 1 >= 0 && nums[resStart] == nums[resStart - 1]){
-                    resStart -= 1;
-                }
-                break;
+                left  = mid + 1;
             }
         }
-        return new int[] {resStart, resEnd};
+        // 得到左侧逼近的极限index后，如果这个index在极限左侧，这个index可能不是nums里面的数字，需要判断这个index的值和target是否相等
+        // 如果这个index在极限右侧，需要判断index是否超出数组
+        if (right == nums.length){
+            return -1;
+        }
+        return  nums[right] == target ? right : -1;
+    }
 
-
+    public int binaryRight(int[] nums, int target){
+        int left = 0;
+        int right = nums.length;
+        while (left < right){
+            int mid = left + (right - left >> 1);
+            if (target >= nums[mid]){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        //如果极限右侧超出数组范围，需要判断left - 1是否等于最右边的那个值
+        //如果极限右侧在最左边，left == 0，说明
+        // if (target >= nums[mid]){
+        //                left = mid + 1;
+        //            }
+        //就没进去过，否则left至少要加个1，left == 0足以证明target不在数组中，在数组左侧，直接返回-1
+        if (left == 0){
+            return -1;
+        }
+        return nums[left - 1] == target ? left - 1 : -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
+    /**
+     * public int[] searchRange(int[] nums, int target) {
+     *         if (nums == null || nums.length == 0){
+     *             return new int[] {-1, -1};
+     *         }
+     *         if (nums.length == 1){
+     *             if (target == nums[0]){
+     *                 return new int[] {0 ,0};
+     *             } else {
+     *                 return new int[] {-1, -1};
+     *             }
+     *         }
+     *         int left = 0, right = nums.length;
+     *         int resStart = -1, resEnd = -1;
+     *         while (left < right){
+     *             int mid = left + (right - left >> 1);
+     *             if (target < nums[mid]){
+     *                 right = mid;
+     *             } else if (target > nums[mid]){
+     *                 left = mid + 1;
+     *             } else {
+     *                 resStart = mid;
+     *                 resEnd = mid;
+     *                 while (resEnd + 1 < nums.length && nums[resEnd] == nums[resEnd + 1]){
+     *                     resEnd += 1;
+     *                 }
+     *                 while (resStart - 1 >= 0 && nums[resStart] == nums[resStart - 1]){
+     *                     resStart -= 1;
+     *                 }
+     *                 break;
+     *             }
+     *         }
+     *         return new int[] {resStart, resEnd};
+     *     }
+     *
+     *     以上方法可以ac，但是以上方法的思路在于，用二分法找到目标数字后，向两侧扩展，假如最坏情况，则数组全部都是同一个数
+     *     扩展的时间复杂度可以到达O（n）
+     *     因此，要想让时间复杂度降低到O（logn），只能采用两次二分，一次找最左边，一次找最右边。
+     */
 }
